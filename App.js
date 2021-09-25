b6a06dd838c5811e9f0dc3cc4679e84f2ff421ac
andreevilela <andrevibegoogle@hotmail.com>
2021-09-25 00:20:18 -0300

    Criação de componente React Native ajustes

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ class App extends Component {
         return (
             <View style={styles.container}>
                 <View style={styles.corpo}>
-                    <Text style={styles.titulo}>Filmes:</Text>
+                    <Text style={styles.titulo}>Filmes</Text>
                     <FlatList
                         data={this.state.filmes}
                         keyExtractor={item => item.id.toString()}
@@ -41,10 +41,11 @@ export default App;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'turquoise'
+        backgroundColor: 'black'
     },
     titulo: {
         fontSize: 30,
+        color: 'white',
         fontWeight: 'bold',
         marginBottom: 20
     },
--- src/Filmes/index.js
+++ src/Filmes/index.js
@@ -1,11 +1,15 @@
 import React, {Component} from "react";
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Image, TouchableOpacity} from 'react-native';
 
 class Filmes extends Component{
     render(){
         return(
             <View>
                 <Text style={styles.nome}>{this.props.data.nome}</Text>
+                <TouchableOpacity onPress={() => alert(this.props.data.nome + '\n\n Sinopse: ' + this.props.data.sinopse)}>
+                    <Image style={styles.foto} source={{uri: this.props.data.foto}} />
+                </TouchableOpacity>
+                <Text style={styles.sinopse}>{'Sinopse: ' + this.props.data.sinopse}</Text>
             </View>
         )
     }
@@ -17,7 +21,28 @@ export default Filmes;
 const styles = StyleSheet.create({
     nome: {
         flex: 1,
-        fontSize: 20
-        
+        fontSize: 18,
+        color: 'white',
+        fontWeight: 'bold',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        marginTop: 3,
+        marginBottom: 15,
     },
+    foto: {
+        width: 200,
+        height: 250,
+        justifyContent: 'center',
+        alignSelf: 'center',
+        marginBottom: 10,
+    }, 
+    sinopse: {
+        flex: 1,
+        fontSize: 15,
+        color: 'white',
+        justifyContent: 'center',
+        marginBottom: 30,
+        textAlign: 'justify'
+
+    }
 });
